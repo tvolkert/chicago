91bc49deaea58fd4834c9d8653d8545e01580f06
Todd Volkert <tvolkert@google.com>
2020-08-11 12:23:05 -0700

    Continued on initial API

--- lib/src/table_view.dart
+++ lib/src/table_view.dart
@@ -18,26 +18,83 @@ import 'package:flutter/rendering.dart' hide TableColumnWidth;
 import 'package:flutter/widgets.dart' hide TableColumnWidth;
 
 import 'basic_table_view.dart';
+import 'scroll_pane.dart';
 import 'sorting.dart';
 
-class TableColumn extends BasicTableColumn {
-  const TableColumn({
-    TableColumnWidth width = const FlexTableColumnWidth(),
-    @required TableCellRenderer cellRenderer,
-    @required this.name,
-    this.sortDirection,
-  }) : super(width: width, cellRenderer: cellRenderer);
+typedef TableHeaderRenderer = Widget Function({BuildContext context, int columnIndex});
 
+class TableColumnController with ChangeNotifier {
+  TableColumnController({
+    this.cellRenderer,
+    this.name,
+    this.headerRenderer,
+    TableColumnWidth width,
+    SortDirection sortDirection,
+  })  : assert(width != null),
+        _width = width;
+
+  final TableCellRenderer cellRenderer;
   final String name;
-  final SortDirection sortDirection;
+  final TableHeaderRenderer headerRenderer;
 
-  TableColumn withRenderer(TableCellRenderer cellRenderer) {
-    return TableColumn(
+  TableColumnWidth _width;
+  TableColumnWidth get width => _width;
+  set width(TableColumnWidth value) {
+    assert(value != null);
+    if (value == _width) return;
+    _width = value;
+    notifyListeners();
+  }
+
+  SortDirection _sortDirection;
+  SortDirection get sortDirection => _sortDirection;
+  set sortDirection(SortDirection value) {
+    assert(value != null);
+    if (value == _sortDirection) return;
+    _sortDirection = value;
+    notifyListeners();
+  }
+}
+
+class ConstrainedTableColumnWidth extends TableColumnWidth {
+  const ConstrainedTableColumnWidth({
+    double width,
+    this.minWidth = 0.0,
+    this.maxWidth = double.infinity,
+  })  : assert(width != null),
+        assert(width >= 0),
+        assert(width < double.infinity),
+        assert(minWidth != null),
+        assert(minWidth >= 0),
+        assert(maxWidth != null),
+        assert(maxWidth >= minWidth),
+        super(width);
+
+  final double minWidth;
+  final double maxWidth;
+
+  ConstrainedTableColumnWidth copyWith({
+    double width,
+    double minWidth,
+    double maxWidth,
+  }) {
+    minWidth ??= this.minWidth;
+    maxWidth ??= this.maxWidth;
+    width ??= this.width;
+    width = width.clamp(minWidth, maxWidth);
+    return ConstrainedTableColumnWidth(
       width: width,
-      cellRenderer: cellRenderer,
-      name: name,
-      sortDirection: sortDirection,
+      minWidth: minWidth,
+      maxWidth: maxWidth,
     );
+  }
+
+  @override
+  @protected
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DoubleProperty('minWidth', minWidth));
+    properties.add(DoubleProperty('maxWidth', maxWidth));
   }
 }
 
@@ -48,11 +105,13 @@ class TableViewHeader extends StatelessWidget {
     Key key,
     this.rowHeight,
     this.columns,
+    this.headerRenderers,
     this.handleColumnResize,
   }) : super(key: key);
 
   final double rowHeight;
-  final List<TableColumn> columns;
+  final List<BasicTableColumn> columns;
+  final List<TableHeaderRenderer> headerRenderers;
   final TableColumnResizeCallback handleColumnResize;
 
   Widget _renderHeader({
@@ -60,6 +119,9 @@ class TableViewHeader extends StatelessWidget {
     int rowIndex,
     int columnIndex,
   }) {
+    final BasicTableColumn column = columns[columnIndex];
+    final bool isColumnResizable = column.width is ConstrainedTableColumnWidth;
+
     return DecoratedBox(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -76,8 +138,13 @@ class TableViewHeader extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Expanded(child: Text(columns[columnIndex].name)),
-          if (columnIndex < columns.length - 1 && handleColumnResize != null)
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 3),
+              child: headerRenderers[columnIndex](context: context, columnIndex: columnIndex),
+            ),
+          ),
+          if (handleColumnResize != null && isColumnResizable)
             SizedBox(
               width: 10,
               child: DecoratedBox(
@@ -109,20 +176,100 @@ class TableViewHeader extends StatelessWidget {
     return BasicTableView(
       rowHeight: rowHeight,
       length: 1,
-      columns: columns.map((TableColumn column) {
-        return column.withRenderer(_renderHeader);
-      }).toList(),
+      columns: List<BasicTableColumn>.generate(columns.length, (int index) {
+        return BasicTableColumn(
+          width: columns[index].width,
+          cellRenderer: _renderHeader,
+        );
+      }),
     );
   }
 }
 
-class TableView extends StatelessWidget {
+class ScrollableTableView extends StatefulWidget {
+  const ScrollableTableView({
+    Key key,
+    this.rowHeight,
+    this.length,
+    this.columns,
+  }) : super(key: key);
+
+  final double rowHeight;
+  final int length;
+  final List<TableColumnController> columns;
+
+  @override
+  _ScrollableTableViewState createState() => _ScrollableTableViewState();
+}
+
+class _ScrollableTableViewState extends State<ScrollableTableView> {
+  List<BasicTableColumn> columns;
+  List<TableHeaderRenderer> headerRenderers;
+
+  void _updateColumns() {
+    setState(() {
+      columns = List<BasicTableColumn>.generate(widget.columns.length, (int index) {
+        return BasicTableColumn(
+          width: widget.columns[index].width,
+          cellRenderer: widget.columns[index].cellRenderer,
+        );
+      });
+      headerRenderers = List<TableHeaderRenderer>.generate(widget.columns.length, (int index) {
+        return widget.columns[index].headerRenderer;
+      });
+    });
+  }
+
+  void _addColumnListener(TableColumnController column) {
+    column.addListener(_updateColumns);
+  }
+
+  void _removeColumnListener(TableColumnController column) {
+    column.removeListener(_updateColumns);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateColumns();
+    widget.columns.forEach(_addColumnListener);
+  }
+
+  @override
+  void dispose() {
+    widget.columns.forEach(_removeColumnListener);
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(ScrollableTableView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _updateColumns();
+    oldWidget.columns.forEach(_removeColumnListener);
+    widget.columns.forEach(_addColumnListener);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BasicTableView(
-      length: null,
-      columns: null,
-      rowHeight: null,
+    return ScrollPane(
+      horizontalScrollBarPolicy: ScrollBarPolicy.expand,
+      verticalScrollBarPolicy: ScrollBarPolicy.auto,
+      columnHeader: TableViewHeader(
+        rowHeight: widget.rowHeight,
+        columns: columns,
+        headerRenderers: headerRenderers,
+        handleColumnResize: (int columnIndex, double delta) {
+          final TableColumnController column = widget.columns[columnIndex];
+          assert(column.width is ConstrainedTableColumnWidth);
+          final ConstrainedTableColumnWidth width = column.width;
+          column.width = width.copyWith(width: width.width + delta);
+        },
+      ),
+      view: BasicTableView(
+        length: widget.length,
+        rowHeight: widget.rowHeight,
+        columns: columns,
+      ),
     );
   }
 }
