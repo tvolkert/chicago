7f451d99326caf3543fd80c5ca6ad76b520395a7
Todd Volkert <tvolkert@google.com>
2020-08-27 17:27:26 -0700

    Made TextInput more configurable

--- lib/src/text_input.dart
+++ lib/src/text_input.dart
@@ -16,26 +16,40 @@
 import 'package:flutter/material.dart';
 
 class TextInput extends StatelessWidget {
+  const TextInput({
+    Key key,
+    this.controller,
+    this.backgroundColor = const Color(0xffffffff),
+    this.obscureText = false,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+
+  final Color backgroundColor;
+
+  final bool obscureText;
+
+  static const InputBorder _inputBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff999999)),
+    borderRadius: BorderRadius.zero,
+  );
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       cursorWidth: 1,
-      cursorColor: Colors.black,
-      style: TextStyle(fontFamily: 'Verdana', fontSize: 11),
+      obscureText: obscureText,
+      cursorColor: const Color(0xff000000),
+      style: const TextStyle(fontFamily: 'Verdana', fontSize: 11),
       decoration: InputDecoration(
-        fillColor: Colors.white,
-        hoverColor: Colors.white,
+        fillColor: backgroundColor,
+        hoverColor: backgroundColor,
         filled: true,
-        contentPadding: EdgeInsets.fromLTRB(3, 13, 0, 4),
+        contentPadding: const EdgeInsets.fromLTRB(3, 13, 0, 4),
         isDense: true,
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Color(0xff999999)),
-          borderRadius: BorderRadius.zero,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Color(0xff999999)),
-          borderRadius: BorderRadius.zero,
-        ),
+        enabledBorder: _inputBorder,
+        focusedBorder: _inputBorder,
       ),
     );
   }
