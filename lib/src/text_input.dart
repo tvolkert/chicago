cace42525049b6c6473a7b579de440be66df0221
Todd Volkert <tvolkert@google.com>
2021-04-29 23:04:59 -0700

    Made TextInput scroll to visible when metrics change if it has the focus

--- lib/src/text_input.dart
+++ lib/src/text_input.dart
@@ -13,11 +13,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 
-class TextInput extends StatelessWidget {
+import 'scroll_pane.dart';
+
+class TextInput extends StatefulWidget {
   const TextInput({
     Key? key,
     this.controller,
@@ -38,68 +42,64 @@ class TextInput extends StatelessWidget {
   final bool enabled;
 
   @override
-  Widget build(BuildContext context) {
-    Widget result = _TextField(
-      controller: controller,
-      focusNode: focusNode,
-      backgroundColor: backgroundColor,
-      obscureText: obscureText,
-      enabled: enabled,
-    );
-
-    if (autofocus) {
-      result = _Autofocus(
-        textField: result as _TextField,
-      );
-    }
-
-    if (onKeyEvent != null) {
-      result = _RawKeyboardEventRepeater(
-        child: result,
-        onKeyEvent: onKeyEvent!,
-      );
-    }
-
-    return result;
-  }
+  _TextInputState createState() => _TextInputState();
 }
 
-class _Autofocus extends StatefulWidget {
-  const _Autofocus({
-    Key? key,
-    required this.textField,
-  }) : super(key: key);
+class _TextInputState extends State<TextInput> with WidgetsBindingObserver {
+  FocusNode? _focusNode;
+  bool _scrollToVisibleScheduled = false;
 
-  final _TextField textField;
+  static const InputBorder _inputBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff999999)),
+    borderRadius: BorderRadius.zero,
+  );
+
+  static const Color _disabledBackgroundColor = Color(0xffdddcd5);
+
+  FocusNode get focusNode => widget.focusNode ?? _focusNode!;
 
   @override
-  _AutofocusState createState() => _AutofocusState();
-}
-
-class _AutofocusState extends State<_Autofocus> {
-  FocusNode? _focusNode;
-
-  FocusNode get focusNode => widget.textField.focusNode ?? _focusNode!;
+  void didChangeMetrics() {
+    if (focusNode.hasFocus) {
+      final ScrollPaneState? scrollPane = ScrollPane.of(context);
+      if (scrollPane != null && !_scrollToVisibleScheduled) {
+        _scrollToVisibleScheduled = true;
+        SchedulerBinding.instance!.addPostFrameCallback((Duration timeStamp) {
+          _scrollToVisibleScheduled = false;
+          if (mounted) {
+            final RenderBox renderObject = context.findRenderObject() as RenderBox;
+            final Rect rect = Offset.zero & renderObject.size;
+            scrollPane.scrollToVisible(rect, context: context);
+          }
+        });
+      }
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    if (widget.textField.focusNode == null) {
+    WidgetsBinding.instance!.addObserver(this);
+    if (widget.focusNode == null) {
       _focusNode = FocusNode();
     }
-    SchedulerBinding.instance!.addPostFrameCallback((Duration timeStamp) {
-      focusNode.requestFocus();
-    });
+    if (widget.autofocus) {
+      SchedulerBinding.instance!.addPostFrameCallback((Duration timeStamp) {
+        if (mounted) {
+          focusNode.requestFocus();
+        }
+      });
+    }
   }
 
   @override
-  void didUpdateWidget(covariant _Autofocus oldWidget) {
+  void didUpdateWidget(covariant TextInput oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.textField.focusNode != oldWidget.textField.focusNode) {
-      if (widget.textField.focusNode == null) {
+    if (widget.focusNode != oldWidget.focusNode) {
+      if (widget.focusNode == null) {
         assert(_focusNode == null);
         _focusNode = FocusNode();
-      } else if (oldWidget.textField.focusNode == null) {
+      } else if (oldWidget.focusNode == null) {
         assert(_focusNode != null);
         _focusNode!.dispose();
         _focusNode = null;
@@ -109,106 +109,25 @@ class _AutofocusState extends State<_Autofocus> {
 
   @override
   void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
     _focusNode?.dispose();
+    _focusNode = null;
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    _TextField result = widget.textField;
-    if (_focusNode != null) {
-      result = result.copyWith(focusNode: _focusNode!);
-    }
-    return result;
-  }
-}
-
-class _RawKeyboardEventRepeater extends StatefulWidget {
-  const _RawKeyboardEventRepeater({
-    Key? key,
-    required this.onKeyEvent,
-    required this.child,
-  }) : super(key: key);
-
-  final ValueChanged<RawKeyEvent> onKeyEvent;
-  final Widget child;
-
-  @override
-  _RawKeyboardEventRepeaterState createState() => _RawKeyboardEventRepeaterState();
-}
-
-class _RawKeyboardEventRepeaterState extends State<_RawKeyboardEventRepeater> {
-  late FocusNode _focusNode;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode = FocusNode();
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return RawKeyboardListener(
-      focusNode: _focusNode,
-      onKey: widget.onKeyEvent,
-      child: widget.child,
-    );
-  }
-}
-
-class _TextField extends StatelessWidget {
-  const _TextField({
-    Key? key,
-    this.focusNode,
-    this.controller,
-    this.backgroundColor = const Color(0xffffffff),
-    this.obscureText = false,
-    this.enabled = true,
-  }) : super(key: key);
-
-  final FocusNode? focusNode;
-  final TextEditingController? controller;
-  final Color backgroundColor;
-  final bool obscureText;
-  final bool enabled;
-
-  _TextField copyWith({
-    required FocusNode focusNode,
-  }) {
-    return _TextField(
-      focusNode: focusNode,
-      controller: controller,
-      backgroundColor: backgroundColor,
-      obscureText: obscureText,
-    );
-  }
-
-  static const InputBorder _inputBorder = OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xff999999)),
-    borderRadius: BorderRadius.zero,
-  );
-
-  static const Color _disabledBackgroundColor = Color(0xffdddcd5);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
+    Widget result = TextField(
+      controller: widget.controller,
       focusNode: focusNode,
       cursorWidth: 1,
-      obscureText: obscureText,
-      enabled: enabled,
+      obscureText: widget.obscureText,
+      enabled: widget.enabled,
       cursorColor: const Color(0xff000000),
       style: const TextStyle(fontFamily: 'Verdana', fontSize: 11),
       decoration: InputDecoration(
-        fillColor: enabled ? backgroundColor : _disabledBackgroundColor,
-        hoverColor: backgroundColor,
+        fillColor: widget.enabled ? widget.backgroundColor : _disabledBackgroundColor,
+        hoverColor: widget.backgroundColor,
         filled: true,
         contentPadding: const EdgeInsets.fromLTRB(3, 13, 0, 4),
         isDense: true,
@@ -217,5 +136,15 @@ class _TextField extends StatelessWidget {
         disabledBorder: _inputBorder,
       ),
     );
+
+    if (widget.onKeyEvent != null) {
+      result = RawKeyboardListener(
+        focusNode: focusNode,
+        onKey: widget.onKeyEvent,
+        child: result,
+      );
+    }
+
+    return result;
   }
 }
